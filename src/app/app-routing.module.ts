729440d4289fc383437fc3875b658a25d0f98bc4
Pike, Laura Beth <lpike6@gatech.edu>
2019-03-17 14:48:38 -0700

    Revert "implement skeleton app"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {FindComponent} from './find/find.component';
-import {ReportComponent} from './report/report.component';
 
-const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'find',
-    pathMatch: 'full'
-  },
-  {
-    path: 'find',
-    component: FindComponent
-  },
-  {
-    path: 'report',
-    component: ReportComponent
-  },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,18 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {AppRoutingModule} from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-// Components
-import {AppComponent} from './app.component';
-import {ReportComponent} from './report/report.component';
-import {FindComponent} from './find/find.component';
-
-// Services
-import {FhirService} from './service/fhir.service';
-
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ReportComponent,
-    FindComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule,
+    AppRoutingModule
   ],
-  providers: [FhirService],
+  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
