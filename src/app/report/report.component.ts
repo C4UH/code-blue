67a710fa6344eb322bf3a4149a38084de24ff414
Laura Pike <laura.pike@gbcnv.edu>
2019-03-27 09:57:44 -0700

    "add jsPDF to report.component"

--- src/app/report/report.component.ts
+++ src/app/report/report.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {FhirService} from '../service/fhir.service';
+import * as jsPDF from 'jspdf';
 
 @Component({
   selector: 'app-report',
@@ -16,7 +17,7 @@ export class ReportComponent implements OnInit {
 
   ngOnInit() {
     this.patientId = this.route.snapshot.queryParamMap.get('patientId');
-    
+
     console.log(`Loading PDMP report for patient ID: ${this.patientId}`);
     this.fhirService.getPDMP(this.patientId)
       .then(res => {
@@ -29,5 +30,24 @@ export class ReportComponent implements OnInit {
    */
   downloadPDF() {
     // TODO: Download PDMP report to PDF
+    const doc = new jsPDF({
+  orientation: 'portrait'
+});
+    var splitText = doc.splitTextToSize(this.reportText,220);
+    var pageHeight = doc.internal.pageSize.height;
+    doc.setFontType("normal");
+    doc.setFontSize("12");
+    var y = 7;
+    for (var i = 0; i < splitText.length; i++) {
+        if (y > 280) {
+            y = 10;
+            doc.addPage();
+        }
+        doc.text(15, y, splitText[i]);
+        y = y + 7;
+    }
+    //doc.text(splitText, 10, 10);
+
+    doc.save('Test.pdf');
   }
 }
