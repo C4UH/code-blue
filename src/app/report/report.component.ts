38b16ab031f57a18dc8e115a5c23c361cea71f1d
Adam Sligar <asligar@gatech.edu>
2019-04-06 10:39:10 -0700

    Finalizes the PDMP layout for webpage and PDF (#63)
    * update package lock file
    
    * add fhir paging, styling, better errors, and better pdf output
    
    * input should work on enter

--- src/app/report/report.component.ts
+++ src/app/report/report.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {FhirService} from '../service/fhir.service';
 import * as jsPDF from 'jspdf';
+import { Medications } from './Medication';
 
 @Component({
   selector: 'app-report',
@@ -10,7 +11,7 @@ import * as jsPDF from 'jspdf';
 })
 export class ReportComponent implements OnInit {
   patientId: string;
-  reportText: string;
+  meds: Medications;
 
   constructor(private route: ActivatedRoute, private fhirService: FhirService) {
   }
@@ -21,7 +22,7 @@ export class ReportComponent implements OnInit {
     console.log(`Loading PDMP report for patient ID: ${this.patientId}`);
     this.fhirService.getPDMP(this.patientId)
       .then(res => {
-        this.reportText = res.reportText;
+        this.meds = res;
       });
   }
 
@@ -29,22 +30,58 @@ export class ReportComponent implements OnInit {
    *  Downloads the PDMP report into a PDF file
    */
   downloadPDF() {
-    const doc = new jsPDF({
+    if(this.meds.error) {
+      alert('Nothing to print');
+    } else {
+      const pdf = this.preparePDF();
+      pdf.save(`pdmp-report_${this.patientId}.pdf`);
+    }
+  }
+
+  preparePDF(): jsPDF {
+    const { medications } = this.meds;
+    const pdf = new jsPDF({
       orientation: 'portrait'
     });
 
-    doc.setFontType('normal');
-    doc.setFontSize('20');
+    pdf.setFontType('normal');
+    pdf.setFontSize('20');
 
-    const pageHeight = doc.internal.pageSize.height || doc.internal.pageSize.getHeight();
-    const pageWidth = doc.internal.pageSize.width || doc.internal.pageSize.getWidth();
+    const pageHeight = pdf.internal.pageSize.height || pdf.internal.pageSize.getHeight();
+    const pageWidth = pdf.internal.pageSize.width || pdf.internal.pageSize.getWidth();
     const title = `PDMP report for Patient ID: ${this.patientId}`;
 
     // Add a title page to the PDF.
-    doc.text(title, pageWidth / 2, pageHeight / 2, 'center')
+    pdf.text(title, pageWidth / 2, pageHeight / 2, 'center')
+    
+    pdf.addPage();
+    pdf.setFontSize('12');
+    
+    // Add text
+    const startOfPage = 15;
+    const leftMargin = 15;
+    const pageLength = 280;
+    let currentPos = startOfPage;
+    for(let i = 0; i < medications.length; i++) {
+      // add new page if we're going over soon
+      if(currentPos > pageLength) {
+        pdf.addPage();
+        currentPos = startOfPage;
+      }
+      // otherwise add text
+      pdf.text(medications[i].name, leftMargin, currentPos);
+      currentPos += 5;
+      for(let j = 0; j < medications[i].codes.length; j++) {
+        pdf.text(medications[i].codes, leftMargin, currentPos);
+        currentPos += 5;
+      }
+      pdf.text(medications[i].date, leftMargin, currentPos);
+      currentPos += 5;
+      pdf.line(0, currentPos, 210, currentPos)
+      currentPos += 7;
+    }
 
-    // Render reportText as html. This means we can use HTML in the report instead of plain text.
-    doc.fromHTML(this.reportText);
-    doc.save(`${this.patientId}.pdf`);  // save the doc to a file
+    return pdf;
   }
+
 }
